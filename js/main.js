1f135355d235b724218d13669bd0f6a716b242c7
simvirk5 <simvirk5@gmail.com>
2018-04-09 11:59:36 -0700

    timer set but not stopping

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function preload() {
     game.load.image('sky', 'assets/images/nightsky.jpg');
     game.load.image('ground', 'assets/images/platform.png');
     game.load.image('enemy', 'assets/images/enemy_box.png');
+    // game.load.image('rainbow', 'assets/images/rainbow.png');
     game.load.spritesheet('unicorn', 'assets/images/dude.png', 32, 48);
 
 }
@@ -12,161 +13,140 @@ function preload() {
 var player;
 var platforms;
 var cursors;
-
-var enemies;
+var enemies = 40;
+// var enemiesMax;
+var coins;
 var score = 0;
 var scoreText;
-var timer;
-var lives;
+var lives = 0;
+var scoreLives;
 var nightsky;
+var gameTimer;
+var timerMax;
 
 function create() {
 
-    //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //  A simple background for our game
     game.add.sprite(0, 0, 'sky');
     nightsky = game.add.tileSprite(0, 0, 800, 600, 'sky');
 
-
-    //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
-
-    //  We will enable physics for any object that is created in this group
     platforms.enableBody = true;
-
-    // Here we create the ground.
     var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
     ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
 
-    // The player and its settings
     player = game.add.sprite(32, game.world.height - 150, 'unicorn');
-    // sprite.anchor.setTo(0.5, 0.5);
-
-    //  We need to enable physics on the player
     game.physics.arcade.enable(player);
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 1;
+    player.body.bounce.y = 0.5;
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
     // player.scale.x = -1;
-
     // player.animations.add('wait', [3,4], 10);
-
-
-    //  Our two animations, walking left and right.
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-    //  Finally some stars to collect
-    enemies = game.add.group();
-    enemies.enableBody = true;
-    enemies.setAll('outOfBoundsKill', true);
-    enemies.setAll('checkWorldBounds', true);
+    enemies = game.add.sprite(0, 0, 'enemy');
+    game.physics.arcade.enable(enemies);
+    enemies.body.gravity.y = 60;
+
+    // coins = game.add.sprite(0, 0,'rainbow');
+    // game.physics.arcade.enable(coins);
+    // coins.scale.setTo(0.1);
+    // coins.body.gravity.y = 20;
+
+
+    // coins.angle = game.rnd.angle();
     
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 50; i++)
-    {
-        //  Create a star inside of the 'stars' group
-
-        // var enemy = enemies.create((Math.random()*300), i*(Math.random()*60), 'enemy' + Math.round(Math.random() * 1));        
-            
-                 // var enemy = enemies.create(i*(Math.random()*60), i*(Math.random()*100), 'enemy');
-
-                var enemy = enemies.create(game.rnd.integerInRange(0, game.world.width - 32), game.rnd.integerInRange(0, game.world.height - 32), 'enemy');   
-
+    // for (var i = 0; i < 50; i++)
+    // {
+        // var enemies = enemies.create(game.rnd.integerInRange(0, game.world.width - 32), game.rnd.integerInRange(0, game.world.height - 32), 'enemy');   
+        // enemies = game.add.sprite(0, 0, 'enemy');
+        // game.physics.arcade.enable(enemies);
+        // enemies.body.gravity.y = 50;
 
         //  Let gravity do its thing
-        enemy.body.gravity.y = 100;
-        // enemy.body.gravity.x = 0;
 
         //  This just gives each star a slightly random bounce value
-        // enemy.body.bounce.y = 0.7 + Math.random() * 0.2;
-    }
 
-
-
-    //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-    //  Our controls.
-    cursors = game.input.keyboard.createCursorKeys();
-
-    lives = game.add.group();
-    game.add.text(game.world.width - 100, 10, 'Lives : ', { font: '34px Arial', fill: '#fff' });
     
-}
+    gameTimer = 500;
+
+    cursors = game.input.keyboard.createCursorKeys();
+    scoreText = game.add.text(16, 16, 'score: 0', { font: '32px Arial', fill: '#fff' });
+    lives = game.add.text(game.world.width - 150, 16, 'Lives : 3 ', { font: '32px Arial', fill: '#fff' });
+    timer = game.add.text(game.world.width - 450, 16, 'Timer : ' + gameTimer, { font: '32px Arial', fill: '#fff' });
+
+ }
+
+
 
 function update() {
 
-        nightsky.tilePosition.x += 2;
+    nightsky.tilePosition.x += 2;
+    gameTimer -= 1; 
+    timer.setText('Timer: ' + gameTimer)   
 
+    if (gameTimer === timerMax) {
 
-    //  Collide the player and the stars with the platforms
-    var hitPlatform = game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(enemies, platforms);
+        console.log('GameOver!')
 
+    }
 
-
-
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.arcade.overlap(player, enemies, collectStar, null, this);
-
-    //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
 
     if (cursors.left.isDown)
     {
-        //  Move to the left
-        player.body.velocity.x = -150;
-
+        player.body.velocity.x = -100;
         player.animations.play('left');
     }
     else if (cursors.right.isDown)
     {
-        //  Move to the right
-        player.body.velocity.x = 150;
-
+        player.body.velocity.x = 100;
         player.animations.play('right');
     }
-    else
-    {
-        //  Stand still
-        player.animations.stop();
+    else {
+    player.animations.stop();
+    player.frame = 4;
 
-        player.frame = 4;
     }
-    
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down && hitPlatform)
-    {
-        player.body.velocity.y = -350;
+        
+    var collide = game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(enemies, coins);
+    game.physics.arcade.overlap(player, enemies, lose, null, this);
+    game.physics.arcade.overlap(player, coins, collectCoins, null, this);
+
+    if (timer <=90000 && timer >= 60000) {
+        enemiesMax = 20;
+            enemies.body.velocity.y +=10;
+            coins.body.velocity.y +=10;
+
+    }
+    else if (timer <=60000 && timer >=30000) {
+        enemiesMx = 15;
+            enemies.body.velocity.y +=10;
+            coins.body.velocity.y +=10;
+    }
+    else if (timer <=30000 && timer >0) {
+        enemiesMax = 10; 
     }
 
 }
 
-
-
-function collectStar (player, enemy) {
-    
-    // Removes the star from the screen
+function lose (player, enemies) {
     player.kill();
-
-
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
-
-
+    enemies.kill();
+    score -= 10;
+    scoreText.setText('Score: ' + score + "Game Over!");
+    console.log("Game Over !");
 }
+
+function collectCoins (player, coin) {
+    coin.kill();
+    lives += 1;
+    scoreLives.setText('Lives: ' + lives);
+}
+
+
 
--- js/menu.js
+++ js/menu.js
@@ -1,36 +1,37 @@
-// //display name of the game and instructions. Wait for user to perform action before calling the next state.
+var menuState = {
 
-// var menuState= {
+	preload: function() {
 
-// 	preload: function() {
-// 		game.load.image()
-// 	}
-// 	create: function(){
+		game.load.image()
+	}
 
-// 		game.physics.startSystem(Phaser.Physics.ARCADE);
+	create: function(){
 
-// 		var title = game.add.sprite('title');
+		game.physics.startSystem(Phaser.Physics.ARCADE);
 
-// 		var titleBox = game.add.sprite(600,90,'titleBox');
+		var title = game.add.sprite('title');
 
-// 		game.physics.ARCADE.enable(this.'titleBox');
+		var titleBox = game.add.sprite(600,90,'titleBox');
 
-// 		game.add.tween(this.titleBox).to({y:80}, 900).start();
+		game.physics.ARCADE.enable(this.'titleBox');
+
+		game.add.tween(this.titleBox).to({y:80}, 900).start();
 		
+		var name = game.add.text(160, 80, "Wobble Box", {
 
-// 		var name = game.add.text(160, 80, "Wobble Box", {
-// 								font: '14px Raleway', fill: '#ffffff'});
+								font: '14px Raleway', fill: '#ffffff'});
 
-// 		var start = game.add.text(160, 80, "Press enter to start")
-// 								font: '14px Raleway', fill: '#ffffff'});
+		var start = game.add.text(160, 80, "Press enter to start")
+		
+								font: '14px Raleway', fill: '#ffffff'});
 
-// 		var enterKey = game.input.keyboard.addkey(Phaser.Keyboard.enter); 
-// 		//function begins when the user presses down the key
-// 		enterKey.onDown.addOnce(this.start, this);
+		var enterKey = game.input.keyboard.addkey(Phaser.Keyboard.enter); 
+		//function begins when the user presses down the key
+		enterKey.onDown.addOnce(this.start, this);
 	
-// 	update: function(){
-// 		if (game.input.activePointer.isDown) {
-// 			game.state.start('play')
-// 		}
-// 	}
-// }
+	update: function(){
+		if (game.input.activePointer.isDown) {
+			game.state.start('play')
+		}
+	}
+}
