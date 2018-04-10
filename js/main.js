ddecbe1f8db3f908c0ac33440fb6fef8729d4a0b
simvirk5 <simvirk5@gmail.com>
2018-04-09 20:16:41 -0700

    timer stops at 0

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ var cursors;
 var enemies;
 // var enemiesMax;
 var coins;
+var coin;
 var score = 0;
 var scoreText;
 var lives = 0;
@@ -54,17 +55,35 @@ function create() {
     enemies.enableBody = true;
     game.physics.arcade.enable(enemies);
     game.physics.arcade.collide(enemies);
+    //create coins
+    // coins = game.add.group();
+    // coins.enableBody = true;
+    // game.physics.arcade.enable(coins);
+    // game.physics.arcade.collide(coins);
+
+///////
+
+    coins = game.add.emitter(game.world.centerX, 0, 100);
+
+    coins.makeParticles('rainbow');
+
+    // coins.minParticleSpeed.setTo(-100, 30);
+    // coins.maxParticleSpeed.setTo(300, 100);
+    // coins.minParticleScale = 0.1;
+    // coins.maxParticleScale = 0.5;
+    // coins.scale.setTo(0.1);
+    // coins.gravity = 50;
+    // coins.flow(1000, 500, 5, );
 
     
     spawEnemy();
 
-    gameTimer = 500;
+    gameTimer = 20;
     cursors = game.input.keyboard.createCursorKeys();
     scoreText = game.add.text(16, 16, 'score: 0', { font: '32px Arial', fill: '#fff' });
     lives = game.add.text(game.world.width - 150, 16, 'Lives : 3 ', { font: '32px Arial', fill: '#fff' });
     timer = game.add.text(game.world.width - 450, 16, 'Timer : ' + gameTimer, { font: '32px Arial', fill: '#fff' });
 
-    gameOver();
  }
 
     //random enemies falling
@@ -78,57 +97,70 @@ function create() {
     }
 }
 
-    //call this function at every instance when game is over
-    function gameOver() {
-        scoreText = game.add.text(16, 16, 'score: 0', 
-        { font: '32px Arial', fill: '#fff'});
-    // game.stage.backgroundColor = '#992d2d';
+    // call this function at every instance when game is over
+//     function gameOver() {
+//         clearInterval(gameTimer);
+//         scoreText = game.add.text(16, 16, 'score: 0', 
+//         { font: '32px Arial', fill: '#fff'});
+//         game.stage.backgroundColor = 'black';
+// }
+    //after 500 destroy timer
+    
+    // function timesUP() {
+        
+    //     game.time.events.add(5000,timer.destroy,timer);
+    // }
 
-}
+       
+    var endTimer = setInterval(function() {
+        gameTimer -= 1;
+        timer.setText('Timer: ' + gameTimer)   
+        console.log('time');
 
-    function update() {
+        if(gameTimer === 0) {
+        console.log('over')
+        clearInterval(endTimer);
+        }},100)
 
-    console.log(enemies.children.length);
+    endTimer()
+
+    function update() {   
+
     //kill the enemy is it's out of the world
+
     enemies.children.forEach((enemy, i) => {
-        if(!enemy.inWorld) {
+    if(!enemy.inWorld) {
             enemy.destroy()
         }
     })
-    //spawn enemy if the array of enemy is less than 1 
+    // spawn enemy if the array of enemy is less than 1 
     if (enemies.children.length < 1) {
         spawEnemy()
     }
 
     //call gameover function in update function
-    gameOver();
+    gameover();
+    
     //background img scroll
     nightsky.tilePosition.x += 2;
-
-    //counting down timer
-    gameTimer -= 1; 
-    timer.setText('Timer: ' + gameTimer)   
 
     //adding rotation to enemies
     enemies.children.forEach(val => {
         val.rotation+=Math.random()*0.2;
     })
-    //if game timer is 0, tell it to end game
-    if (gameTimer <=0 ) {
-        gameOver();
-    }
+
     player.body.velocity.x = 0;
 
     //make the player move 
 
     if (cursors.left.isDown)
     {
-        player.body.velocity.x = -100;
+        player.body.velocity.x = -150;
         player.animations.play('left');
     }
     else if (cursors.right.isDown)
     {
-        player.body.velocity.x = 100;
+        player.body.velocity.x = 150;
         player.animations.play('right');
     }
     else {
@@ -144,31 +176,29 @@ function create() {
     game.physics.arcade.overlap(player, coins, collectCoins, null, this);
 
     //change velocity and amount of enemies as time decreases
-    if (timer <=500 && timer >= 300) {
+    if (endTimer <=500 && timer >= 300) {
             maxEnemies;
             enemies.body.velocity.y = 50;
             // coins.body.velocity.y +=10;
     }
     else if (timer <=300 && timer >=200) {
-            maxEnemies = 15;
+            maxEnemies +=10;
             enemies.body.velocity.y = 40;
             coins.body.velocity.y +=10;
     }
     else if (timer <=200 && timer >0) {
-            enemiesMax = 10; 
+            enemiesMax = 5; 
             enemies.body.velocity.y = 25;
-
     }
-
 }
 
-    //collsion between player and enemies
-function lose (player, enemies) {
-    player.kill();
-    enemies.kill();
-    score -= 10;
-    gameOver();
-}
+//collsion between player and enemies
+    function lose (player, enemies) {
+        player.kill();
+        enemies.kill();
+        score -= 10;
+        gameOver();
+    }
 
 //collision between player and coins
 function collectCoins (player, coin) {
