d8223a8bef974e20a725c9e75b7638d9068e3e60
simvirk5 <simvirk5@gmail.com>
2018-04-09 17:41:56 -0700

    rotate boxes and loop them

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function preload() {
     game.load.image('sky', 'assets/images/nightsky.jpg');
     game.load.image('ground', 'assets/images/platform.png');
     game.load.image('enemy', 'assets/images/enemy_box.png');
-    // game.load.image('rainbow', 'assets/images/rainbow.png');
+    game.load.image('rainbow', 'assets/images/rainbow.png');
     game.load.spritesheet('unicorn', 'assets/images/dude.png', 32, 48);
 
 }
@@ -13,7 +13,7 @@ function preload() {
 var player;
 var platforms;
 var cursors;
-var enemies = 40;
+var enemies;
 // var enemiesMax;
 var coins;
 var score = 0;
@@ -22,73 +22,103 @@ var lives = 0;
 var scoreLives;
 var nightsky;
 var gameTimer;
+var maxEnemies = 15;
+var enemy;
 
 function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
-
+    
+    game.stage.backgroundColor = 'black';
+    //backgorund sky
     game.add.sprite(0, 0, 'sky');
     nightsky = game.add.tileSprite(0, 0, 800, 600, 'sky');
-
+    //create platform
     platforms = game.add.group();
     platforms.enableBody = true;
     var ground = platforms.create(0, game.world.height - 64, 'ground');
     ground.scale.setTo(2, 2);
     ground.body.immovable = true;
-
+    //create player
     player = game.add.sprite(32, game.world.height - 150, 'unicorn');
     game.physics.arcade.enable(player);
-    player.body.bounce.y = 0.5;
+    player.body.bounce.y = 1;
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
-    // player.scale.x = -1;
-    // player.animations.add('wait', [3,4], 10);
+    //player animations
+    player.animations.add('wait', [3,4], 10);
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-    enemies = game.add.sprite(0, 0, 'enemy');
+    //create enemies
+    enemies = game.add.group();
+    enemies.enableBody = true;
     game.physics.arcade.enable(enemies);
-    enemies.body.gravity.y = 60;
+    game.physics.arcade.collide(enemies);
 
-    // coins = game.add.sprite(0, 0,'rainbow');
-    // game.physics.arcade.enable(coins);
-    // coins.scale.setTo(0.1);
-    // coins.body.gravity.y = 20;
-
-
-    // coins.angle = game.rnd.angle();
+    //random player falling
     
-    // for (var i = 0; i < 50; i++)
-    // {
-        // var enemies = enemies.create(game.rnd.integerInRange(0, game.world.width - 32), game.rnd.integerInRange(0, game.world.height - 32), 'enemy');   
-        // enemies = game.add.sprite(0, 0, 'enemy');
-        // game.physics.arcade.enable(enemies);
-        // enemies.body.gravity.y = 50;
+    spawEnemy();
 
-        //  Let gravity do its thing
+    console.log(enemy);
 
-        //  This just gives each star a slightly random bounce value
-
-    
     gameTimer = 500;
-
     cursors = game.input.keyboard.createCursorKeys();
     scoreText = game.add.text(16, 16, 'score: 0', { font: '32px Arial', fill: '#fff' });
     lives = game.add.text(game.world.width - 150, 16, 'Lives : 3 ', { font: '32px Arial', fill: '#fff' });
     timer = game.add.text(game.world.width - 450, 16, 'Timer : ' + gameTimer, { font: '32px Arial', fill: '#fff' });
 
+    gameOver();
  }
+
+function spawEnemy () {
+    for (var i = 12; i > 0; i--) {
+        enemy = enemies.create(Math.random()*game.world.width, 0, 'enemy');
+        enemy.body.gravity.y = game.rnd.integerInRange(50, 100);
+        enemy.checkWorldBounds = true;
+        enemy.outOfBoundsKill = true;
+    }
+}
+
+function checkEnemy () {
+    // enemies.children.forEach((enemy, i) => {
+    //    if(enemy.destroy();
+    // })
+}
 
 function gameOver() {
     scoreText = game.add.text(16, 16, 'score: 0', 
     { font: '32px Arial', fill: '#fff'});
+    // game.stage.backgroundColor = '#992d2d';
+
 }
 
 function update() {
 
+    console.log(enemies.children.length);
+
+    enemies.children.forEach((enemy, i) => {
+        if(!enemy.inWorld) {
+            enemy.destroy()
+        }
+    })
+
+    if (enemies.children.length < 1) {
+        spawEnemy()
+    }
+
+    gameOver();
+    //background img scroll
     nightsky.tilePosition.x += 2;
+
+    //counting down timer
     gameTimer -= 1; 
     timer.setText('Timer: ' + gameTimer)   
+
+    //adding rotation to enemies
+    enemies.children.forEach(val => {
+        val.rotation+=Math.random()*0.2;
+    })
+
 
     if (gameTimer <=0 ) {
         gameOver();
@@ -96,6 +126,7 @@ function update() {
 
     player.body.velocity.x = 0;
 
+    //make the player move 
     if (cursors.left.isDown)
     {
         player.body.velocity.x = -100;
@@ -112,11 +143,14 @@ function update() {
 
     }
         
-    var collide = game.physics.arcade.collide(player, platforms);
+    //collision between groups 
+    game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(enemies);
     game.physics.arcade.collide(enemies, coins);
     game.physics.arcade.overlap(player, enemies, lose, null, this);
     game.physics.arcade.overlap(player, coins, collectCoins, null, this);
 
+    //change velocity and amount of enemies as time decreases
     if (timer <=90000 && timer >= 60000) {
         enemiesMax = 20;
             enemies.body.velocity.y +=10;
@@ -134,6 +168,7 @@ function update() {
 
 }
 
+    //collsion between player and enemies
 function lose (player, enemies) {
     player.kill();
     enemies.kill();
@@ -141,6 +176,7 @@ function lose (player, enemies) {
     gameOver();
 }
 
+//collision between player and coins
 function collectCoins (player, coin) {
     coin.kill();
     lives += 1;
